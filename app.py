b2cff7116cb1a74399f7f8c2f927854485cb75a3
Lalesca Nascimento <143019186+laahnascimento@users.noreply.github.com>
2023-09-08 15:03:57 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+media=float(input("Qual a sua nota:"))
+   
+   if (media>=60):
+    print("Você foi aprovada!")
+
+   elif media<40:
+    print("Você foi reprovada.")
+   
+   else:
+    print("Você está de recuperação.")
+   
